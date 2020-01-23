93b812dbfc2760394526f1c0397ea27df7cc4b99
Sungmin Cho <abelsteiger@gmail.com>
2020-01-23 14:16:48 -0800

    mod - player movements touch reaction

---
+++ Assets/Scripts/Camera/CameraOverlay.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOverlay : MonoBehaviour
+{
+    public Material overlayMaterial;
+    public Color materialColor;
+    public Color colorChangeTarget;
+
+    public int colorChangeCounterStart;
+    public int colorChangeStartThreshold;
+    public float colorChangeCounter;
+    public float colorChangeThreshold;
+    public float RMax;
+    public float RMin;
+    public float GMax;
+    public float GMin;
+    public float BMax;
+    public float BMin;
+    public float colorChangeSpeed;
+    public bool colorChangeInMotion;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        colorChangeCounter++;
+        colorChangeCounterStart++;
+        
+        overlayMaterial.SetColor("_Color", materialColor);
+        if(colorChangeCounterStart > colorChangeStartThreshold){
+            ColorChanger();
+        }
+    }
+
+    void ColorChanger(){
+        if(colorChangeCounter > colorChangeThreshold && !colorChangeInMotion){
+            colorChangeInMotion = true;
+            colorChangeTarget = new Color(Random.Range(RMin, RMax),Random.Range(GMin, GMax),Random.Range(BMin, BMax),0.5f);
+        }
+
+        if(colorChangeInMotion){
+
+            materialColor = Color.Lerp(materialColor,colorChangeTarget ,colorChangeSpeed);
+            
+            float diff = Vector4.Distance(materialColor,colorChangeTarget);
+            
+            if(Mathf.Abs(diff) < 0.01f){
+                colorChangeInMotion = false;
+                colorChangeCounter = 0;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager/RingInstantiateScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingInstantiateScript : MonoBehaviour
+{
+    public float ringSpeed;
+    public bool instantiateFlag;
+
+    public List<GameObject> arrayLevel_0;
+    public List<GameObject> arrayLevel_1;
+    public List<GameObject> arrayLevel_2;
+    public GameObject ringTest;
+
+    private GameObject currentInstance;
+
+    public Metronome metronomeScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        metronomeScript = this.GetComponent<Metronome>();
+        arrayLevel_0 = PopulateInstantiateList(0);
+        arrayLevel_1 = PopulateInstantiateList(1);
+        arrayLevel_2 = PopulateInstantiateList(2);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(metronomeScript.beat){
+            instantiateFlag = true;
+            metronomeScript.beat = false;
+        }
+        
+        if(instantiateFlag){
+            //Level selector
+            GameObject tempObject = PickFromLevel(0);
+            
+            //instantiating tempObject
+            currentInstance = GameObject.Instantiate(tempObject,Vector3.zero,Quaternion.identity);
+            // Rotate on create
+            // currentInstance.GetComponent<RotationScript>().rotationSpeed = 0;
+            // currentInstance.GetComponent<RotationScript>().randomInitial = false;
+            
+            instantiateFlag = false;
+        }
+    }
+
+    GameObject PickFromLevel(int level){
+        switch(level){
+            case 0:
+                {
+                    //Picker Logic
+                    return arrayLevel_0[Random.Range(0,arrayLevel_0.Count)];
+                }
+            default:
+                return ringTest;
+        }
+    }
+
+    public void spawnRing() {
+        instantiateFlag = true;
+    }
+
+    public List<GameObject> PopulateInstantiateList(int inputLevel){
+        
+        List<GameObject> returnArray = new List<GameObject>();
+        
+        foreach(GameObject gameObject in Resources.LoadAll("Level"+inputLevel))
+        {
+        returnArray.Add(gameObject);
+        }
+
+        return returnArray;
+    }
+
+
+}
