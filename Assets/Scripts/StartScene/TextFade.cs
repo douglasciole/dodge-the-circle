a23f39e34ff377a57b796b170d19824ccbf0e522
Sungmin Cho <abelsteiger@gmail.com>
2020-01-23 15:31:52 -0800

    add - Start scene added

---
+++ Assets/Scripts/StartScene/TextFade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour
+{
+    public Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        StartCoroutine(FadeTextToFullAlpha());
+    }
+
+    public IEnumerator FadeTextToFullAlpha()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+        while (text.color.a < 1.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + (Time.deltaTime / 1.0f));
+            yield return null;
+        }
+        StartCoroutine(FadeTextToZeroAlpha());
+    }
+
+    public IEnumerator FadeTextToZeroAlpha()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / 1.0f));
+            yield return null;
+        }
+        StartCoroutine(FadeTextToFullAlpha());
+    }
+}
